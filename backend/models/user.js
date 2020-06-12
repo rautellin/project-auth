e229f0655f59cdac1ad89889194eeee1de75572d
Nathalie Rautell <nathalierautell@Nathalies-MacBook-Pro.local>
2020-06-12 20:57:09 +0200

    set up the backend with routes and models

---
+++ backend/models/user.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose'
+import crypto from 'crypto'
+
+const User = mongoose.model('User', {
+  name: {
+    type: String,
+    unique: true
+  },
+  email: {
+    type: String,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString('hex')
+  }
+})
+
+export default User
--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,14 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import bcrypt from 'bcrypt-nodejs'
+import crypto from 'crypto'
+import User from './models/user'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
 
@@ -18,9 +17,52 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
-// Start defining your routes here
+// Add middleware to aythenticate user
+const authenticateUser = async (req, res, next) => {
+  const user = await User.findOne({ accessToken: req.header('Authorization') })
+
+  if (user) {
+    req.user = user
+    next()
+  } else {
+    res.status(401).json({ loggedOut: true })
+  }
+}
+
+// ERROR MESSAGES
+const ERR_CANNOT_FIND_ID = 'User not found.'
+const ERR_CANNOT_CREATE = 'Could not create user.'
+
+// ROUTES
 app.get('/', (req, res) => {
   res.send('Hello world')
+})
+
+// Create a user
+app.post('/users', async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const user = new User({ name, email, password: bcrypt.hashSync(password) });
+    const createdUser = await user.save();
+
+    res.status(201).json({ userId: createdUser._id, accessToken: createdUser.accessToken });
+  } catch (err) {
+    res.status(400).json({ message: ERR_CANNOT_CREATE, errors: err });
+  }
+})
+
+app.get('/secrets', authenticateUser)
+app.get('/secrets', (req, res) => {
+  res.json({ secret: 'This is secret' })
+})
+
+app.post('/sessions', async (req, res) => {
+  const user = await User.findOne({ email: req.body.email })
+  if (user && bcrypt.compareSync(req.body.password, user.password)) {
+    res.json({ userId: user._id, accessToken: user.accessToken })
+  } else {
+    res.json({ notFound: true })
+  }
 })
 
 // Start the server
